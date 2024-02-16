4f7389d47e4b28535a4e3b2e8d705d4f600889aa
Dmitrii Larionov <inxo@inxo.ru>
2024-02-16 17:44:54 +0300

    fix

--- src/main.go
+++ src/main.go
@@ -6,18 +6,18 @@ import (
 	"log"
 	"net/http"
 	"time"
-	functions2 "viads-cron-plugin/src/functions"
+	"viads-cron-plugin/src/functions"
 )
 
 func Handle(params go_ignt_func_deps.ParamsInterface, _ *http.Request) go_ignt_func_deps.ResponseInterface {
 	hello, err := params.Get("hello")
-	functions2.CheckErr(err)
+	functions.CheckErr(err)
 	second, err := params.Get("second")
-	functions2.CheckErr(err)
+	functions.CheckErr(err)
 	log.Println("cll test_f")
 	jsonData, err := json.Marshal("hello " + hello.Value + " second: " + second.Value + " when:" + time.Now().String())
-	functions2.CheckErr(err)
-	return functions2.JsonResponse{Data: string(jsonData)}
+	functions.CheckErr(err)
+	return functions.JsonResponse{Data: string(jsonData)}
 }
 
 func GetParams() []string {
